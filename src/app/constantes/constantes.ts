dfe99e38c032b79829d85811f7bf4569138b194e
abarrios0209 <80713264+abarrios0209@users.noreply.github.com>
2024-11-28 19:45:49 -0500

    Corrección del mensaje

--- src/app/constantes/constantes.ts
+++ src/app/constantes/constantes.ts
@@ -4,18 +4,15 @@ export class ConstantesApp {
   static personajeConMasDisLikes: string = 'Personaje con más Dislikes';
   static estadoPikachu: string = 'Personaje con más Likes';
 
-
-  static errorPersonajeAleatorio: string = 'Personaje con más Likes';
+  static errorPersonajeAleatorio: string = 'Error al obtener el personaje aleatorio';
   static errorPersonajeMasLikes: string = 'Error al obtener el personaje con mas likes';
   static errorPersonajeMasDisLikes: string = 'Error al obtener el personaje con mas dislikes';
   static errorEstadoPikachu: string = 'Error al obtener el estado del Pikachu';
   static errorAlDarLike: string = 'Error al dar like';
   static errorAlDarDisLike: string = 'Error al dar dislike';
   
-  
   static likeInsertado: string = 'Like insertado exitosamente';
   static dislikeInsertado: string = 'DisLike insertado exitosamente';
-  
   
   static selectLikes: string = 'likes';
   static selectDisLikes: string = 'dislikes';
